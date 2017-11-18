75de46af6036a2d2806896ea2cbdacefb049d5f7
bobby569 <zhan2175@purdue.edu>
2017-11-17 23:40:20 -0500

    done

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import PostsReducer from './PostsReducer';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+	form: formReducer,
+	posts: PostsReducer
 });
 
 export default rootReducer;
